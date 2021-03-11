25162d2a55ad5b3839feeeb9bdc4f34eb832a9bd
Fei Rao <raof01@gmail.com>
2021-03-11 15:01:29 -0800

    Update

--- knowledge_test.py
+++ knowledge_test.py
@@ -24,33 +24,37 @@ def fetch_available_times(loc):
 	ctx = ssl.create_default_context()
 	ctx.check_hostname = False
 	ctx.verify_mode = ssl.CERT_NONE
-	url = "https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/branches/{0}/dates;servicePublicId={1};customSlotLength=15"
+	url = "https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/branches/{0}/dates;servicePublicId={1};customSlotLength=40"
 	url =  url.format(loc['id'], servicePublicId)
 	response = urllib2.urlopen(url, context=ctx)
 	content = response.read()
 	return json.loads(content)
 
 # Match month.
-def is_match(times, month):
+def get_match(times, month):
+	ret = []
 	for time in times:
 		d = time['date']
 		if d.startswith(month):
-			return True
-	return False
+			ret.append(time)
+	return ret
 
 def send_notification(times, loc):
 	# TODO: add notification to mobile or desktop.
-	make_bell_sound()
-	print 'Found match date at [', loc["name"],']'
-	print 'Available times'
-	for t in times:
-		print t['date']
+	if times:
+		make_bell_sound(loc["name"])
+		print 'Found match date at [', loc["name"],']'
+		print 'Available times'
+		# for t in times:
+		# 	print t['date']
+		print [t['date'] for t in times]
 
-# make a sound in your computer 
-def make_bell_sound():
-	os.system("say 'ICBC appointment found.'")
-	os.system("say 'ICBC appointment found.'")
-	os.system("say 'ICBC appointment found.'")
+# make a sound in your computer
+def make_bell_sound(loc):
+	str = "say 'ICBC appointment found at {0}.'".format(loc)
+	os.system(str)
+	os.system(str)
+	os.system(str)
 
 def main():
 	expectMonth = sys.argv[2]
@@ -59,15 +63,12 @@ def main():
 	locations = read_location_json(loc_file)
 	try_time = 1
 	for loc in locations:
-		while True:
-			times = fetch_available_times(loc)
-			if is_match(times, expectMonth):
-				send_notification(times, loc)
-				break
-			time.sleep(3)
-			try_time += 1 
-			sys.stdout.write(".")
-			sys.stdout.flush()
+		times = fetch_available_times(loc)
+		send_notification(get_match(times, expectMonth), loc)
+		time.sleep(3)
+		try_time += 1
+		sys.stdout.write(".")
+		sys.stdout.flush()
 
 
 if __name__ == "__main__":
